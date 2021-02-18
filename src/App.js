2cadb9bfc5c21c17811a1c132fbfdb94242eae82
Rayhan1998 <miahrayhan01@gmail.com>
2021-02-17 19:43:55 -0500

    feat:created header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar">
+        <div class="logo">
+          <h1>Movie Finder</h1>
+        </div>
+
+        <ul class="list">
+          <li class="list-item">
+            <a href="#" class="list-link">
+              My Watchlist
+            </a>
+          </li>
+          <li class="list-item">
+            <a href="#" class="list-link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
