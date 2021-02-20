3f98ceefe5f59401b14f556802acf886103a23b7
Rayhan1998 <miahrayhan01@gmail.com>
2021-02-19 19:04:14 -0500

    setup more info and add to watchlist html/css

--- src/components/MovieBoxes.js
+++ src/components/MovieBoxes.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 export default function MovieBoxes({ id, movie }) {
+  const imageTemplate = "https://image.tmdb.org/t/p/w300";
   return (
     <div className="card">
-      <h1>{movie.original_title}</h1>
+      <img
+        className="image"
+        src={`${imageTemplate}${movie.poster_path}`}
+        alt="movie-image"
+      />
+      <h1 style={{ paddingTop: "10px", textAlign: "center" }}>
+        {movie.original_title}
+      </h1>
+      <button className="button is-primary card-btn">more info</button>
+      <button className="button is-primary  card-btn">add to watchlist</button>
     </div>
   );
 }
